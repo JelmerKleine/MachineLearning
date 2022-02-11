81c32ab3b101c6c86f9162dd4ca8acd466ad198d
JelmerKleine <jelmerkleine@gmail.com>
2022-02-11 11:26:17 +0100

    added assignment 1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+import numpy as np
+X_b = np.c_[np.ones((100, 1)), X] # add x0 = 1 (bias) to each obs.
+theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+print(theta)
 
 
 def print_hi(name):
